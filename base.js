c948930bdae469e19622b4f00f33ede9be775ab5
munrhalls <antarcticdepths@gmail.com>
2020-11-29 15:42:08 +0100

    access clock elements w/ js. update style /w time data. transform -> rotate

--- base.js
+++ base.js
@@ -3,5 +3,14 @@ setInterval(function(){
   var seconds = time.getSeconds();
   var minutes = time.getMinutes(); 
   var hours = time.getHours(); 
-  console.log(seconds)
+
+  var hour = document.getElementsByClassName('hour')[0];
+  var minute = document.getElementsByClassName('minute')[0];
+
+  var second = document.getElementsByClassName('second')[0];
+
+  hour.style.transform = 'rotate(' + (hours * 6) +'deg)';
+  minute.style.transform = 'rotate(' + (minutes * 6) +'deg)';
+  second.style.transform = 'rotate(' + (seconds * 6) +'deg)';
+  
 }, 1000);
